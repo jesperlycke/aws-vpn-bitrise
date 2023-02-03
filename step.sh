9c6020213fae4495245c147fe5b2569da00ca19e
Jesper Lycke <jesper.lycke@loomispay.com>
2023-02-03 09:58:21 +0100

    ifconfig

--- step.sh
+++ step.sh
@@ -50,7 +50,7 @@ EOF
     echo "Before sleep"
     sleep 5s
     echo "After sleep"
-    echo ifconfig
+    ifconfig
     if ! ifconfig | grep tun0 > /dev/null ; then
       echo "No open VPN tunnel found"
       cat "$log_path"
