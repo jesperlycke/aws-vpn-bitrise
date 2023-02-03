3f788a7bf59f0b6f564654abfc7b5d570bb022f1
Jesper Lycke <jesper.lycke@loomispay.com>
2023-02-03 11:25:26 +0100

    client.status

--- step.sh
+++ step.sh
@@ -58,7 +58,7 @@ EOF
     sleep 10s
     ifconfig
     ps aux | grep openvpn
-    cat /run/openvpn/status
+    cat /run/openvpn/client.status
     echo "After sleep"
     if ! ifconfig | grep tun0 > /dev/null ; then
       echo "No open VPN tunnel found"
